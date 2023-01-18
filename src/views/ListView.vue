bffbd17de20f4058feb0035efb3e693ec65870bb
Hector Nagao <hectornagao@hotmail.com>
2023-01-18 10:19:09 -0300

    configured json server fake backend

--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -9,5 +9,23 @@
 
   export default {
     name: 'ListView',
+    data(){
+      return {
+        people:[],
+        api: 'http://localhost:8080/api'
+      }
+    },
+    methods: {
+      
+      getPeople: function(){
+        this.$http.get(this.api+"/people").then((res)=>{
+          this.people = res.data;
+          console.log(this.people)
+        })
+      }
+    },
+    mounted(){
+      this.getPeople()
+    }
   }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,21 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  devServer:{
+    proxy:{
+      '^/api': {
+
+      target: 'http://localhost:3000',
+
+      changeOrigin: true,
+
+      secure:false,
+
+      pathRewrite: {'^/api': ''},
+
+      logLevel: 'debug'
+      },
+  }
+  }
 })
